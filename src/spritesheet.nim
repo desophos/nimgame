4535aad5d8ae96388fbf9f822d9b6e76978918bc
Daniel Horowitz <daniel.horowitz@openx.com>
2015-10-21 14:59:12 -0700

    pascalCase, not underscores...

--- src/spritesheet.nim
+++ src/spritesheet.nim
@@ -13,16 +13,16 @@ type SpriteSheet* = object of RootObj
 proc spriteSheet*(ren: sdl2.RendererPtr, file: string, size: Size): SpriteSheet =
   let
     sheet = drawable(ren, file)
-    sheet_size = sheet.getSize
+    sheetSize = sheet.getSize
   # sprites are defined by Views into a SpriteSheet
   # views are in reading order
   # (in rows, left to right, top to bottom)
   var
     views: seq[View] = @[]
     curX, curY: int
-  for y in 0 ..< int(math.ceil(sheet_size.h / size.h)):
+  for y in 0 ..< int(math.ceil(sheetSize.h / size.h)):
     curX = 0
-    for x in 0 ..< int(math.ceil(sheet_size.w / size.w)):
+    for x in 0 ..< int(math.ceil(sheetSize.w / size.w)):
       views.add(view(curX, curY, size.w, size.h))
       curX += size.w
     curY += size.h
